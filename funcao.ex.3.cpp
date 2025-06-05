a3b78275315744e8657c9a4e4cb59f9c8c0e8cb1
Samuel <s4mueljacome@outlook.com>
2025-06-05 09:52:47 -0300

    criado a main para testar

--- funcao.ex.3.cpp
+++ funcao.ex.3.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 
 enum Coluna {
-    A, B, C, D, E, F, G, H
+    A, B, C, D, E, F, G, H // enumerando as casas e colunas
 };
 struct Ponto {
     Coluna coluna; // de A a H (valores de 0 a 7)
     int linha;      // de 0 a 7 (linha 1 é 0, linha 8 é 7)
 };
 
+//tranformar a minha entrada que esta em string em um ponto da matriz
 Ponto lerPosicao(const string& entrada) {
-    Ponto p;
-    p.coluna = Coluna(entrada[0] - 'a'); // 'c' - 'a' = 2
+    Ponto p; // criar um ponto p
+    p.coluna = Coluna(entrada[0] - 'a'); // 'c' - 'a' = 2 // vai dar o numero da coluna que 
     p.linha = entrada[1] - '1';          // '2' - '1' = 1
-    return p;
+    return p; //vai retornar o ponto que corresponde a string
 }
 
 bool rainha (Ponto origem, Ponto destino, int tabuleiro[8][8]){
@@ -46,32 +47,45 @@ bool rainha (Ponto origem, Ponto destino, int tabuleiro[8][8]){
 
 }
 
+int main () {  
+    string start;
+    string end;
 
+    cin >> start;
+    cin >> end;
 
+    int tabuleiro[8][8] = {0};
 
+    Ponto inicio = lerPosicao(start);
+    Ponto final = lerPosicao(end);
 
+    
+    cout << rainha (inicio, final, tabuleiro) << '\n';
 
-
-
-
-
-
-
-
-
-
-
-
-int main () {
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
